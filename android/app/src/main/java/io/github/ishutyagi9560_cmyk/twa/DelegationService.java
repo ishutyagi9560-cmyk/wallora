556dc52fed3d58427f3035dbd32dd286ffa790dc
ishutyagi9560-cmyk <ishutyagi9560-cmyk@users.noreply.github.com>
2026-09-03 12:12:43 +0530

    Add Android build workflow

---
+++ android/app/src/main/java/io/github/ishutyagi9560_cmyk/twa/DelegationService.java
@@ -0,0 +1,14 @@
+package io.github.ishutyagi9560_cmyk.twa;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
